b8c20924b082c1d3d463530cd25dcbc9a13ccf97
igorKAznov <malverdo@yandex.ru>
2023-08-18 12:20:02 +0300

    fix handler and env

--- systemeio/src/Application/UseCase/CommandHandler.php
+++ systemeio/src/Application/UseCase/CommandHandler.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace App\Application\UseCase;
 
-use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
-
-interface CommandHandler extends MessageHandlerInterface
+interface CommandHandler
 {
 }
